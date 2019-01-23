81ea8bb1d1f0f85e4105c073d00eb0efe91cd68e
princewen <princewen2019@outlook.com>
2019-01-23 16:57:47 +0800

    lecture 2

--- 序列构成的数组/列表推导和生成器表达式.py
+++
@@ -1,8 +0,0 @@
-# 列表推导式
-print([x for x in range(4)]) # [0, 1, 2, 3]
-
-# 在python2中，列表推导式中for关键词之后的赋值操作可能会影响列表推导上下文中的同名变量，但是python3已经修复
-x = 'my precious'
-dummy = [x for x in 'ABC']
-print(x) # python2输出'C'，python3输出my precious
-
